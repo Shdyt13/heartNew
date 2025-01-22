1d1d47b8f1408abdd9a4c6b65dfef0e7aeddd673
Shdyt.S <shdyt.s2004@gmail.com>
2025-01-23 01:27:10 +0700

    Struktur Folder

---
+++ static/script.js
@@ -0,0 +1,12 @@
+function checkName() {
+    const inputName = document.getElementById('nameInput').value;
+    const correctName = 'Qurratu Aini Sukma';
+
+    if (inputName === correctName) {
+        // Jika nama benar, arahkan ke halaman love.html
+        window.location.href = '../love.html';
+    } else {
+        // Jika nama salah, arahkan ke halaman wrong.html
+        window.location.href = '../wrong.html';
+    }
+}
